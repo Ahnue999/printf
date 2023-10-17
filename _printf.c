b9ab5146a97bdae1b34d730d23e8930891c4145f
Ahnue999 <ahnue200572@gmail.com>
2023-10-17 01:31:36 -0700

    Betty optimization

--- _printf.c
+++ _printf.c
@@ -14,27 +14,22 @@ int _printf(const char *format, ...)
 	char *buf;
 	int (*func)(va_list, char *, unsigned int);
 
-	va_start(list, format);
-
-	buf = malloc(sizeof(char) * BUFSIZE);
+	va_start(list, format), buf = malloc(sizeof(char) * BUFSIZE);
 	if (!format || (format[i] == '%' && !format[i + 1]) || !buf)
 		return (-1);
 	if (!format[i])
 		return (0);
-
 
 	for (i = 0; format && format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
-			{
-				print_buf(buf, pos), free(buf), va_end(list);
+			{	 print_buf(buf, pos), free(buf), va_end(list);
 				return (-1);
 			}
 			else
-			{
-				func = get_func(format, i + 1);
+			{	func = get_func(format, i + 1);
 				if (func == NULL)
 				{
 					if (format[i + 1] == ' ' && !format[i + 2])
@@ -42,8 +37,7 @@ int _printf(const char *format, ...)
 					input_buf(buf, format[i], pos), ch++, i--;
 				}
 				else
-				{
-					ch += func(list, buf, pos);
+				{	ch += func(list, buf, pos);
 					i += ev_print_func(format, i + 1);
 				}
 			} i++;
@@ -53,10 +47,6 @@ int _printf(const char *format, ...)
 		for (pos = ch; pos > BUFSIZE; pos -= BUFSIZE)
 			;
 	}
-
-	print_buf(buf, pos);
-	free(buf);
-	va_end(list);
-
+	print_buf(buf, pos), free(buf), va_end(list);
 	return (ch);
 }
