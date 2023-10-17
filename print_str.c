3e3d86935343bf6f161f5521379391ed5b4298ed
Ahnue999 <ahnue200572@gmail.com>
2023-10-17 10:47:50 -0700

    rot13

--- print_str.c
+++ print_str.c
@@ -29,3 +29,75 @@ int print_str(va_list list, char *buf, unsigned int pos)
 
 	return (i);
 }
+
+/**
+ * print_rev - concatenates the provided string
+ * to the buffer in reverse.
+ *
+ * @list: contains the string to add.
+ * @buf: the buffer.
+ * @pos: the position to concatenate to.
+ * Return: the length of the printed string.
+ */
+int print_rev(va_list list, char *buf, unsigned int pos)
+{
+	char *s;
+	unsigned int i, j = 0;
+	char *nil = "(null)";
+
+	s = va_arg(list, char *);
+
+	if (s == NULL)
+	{
+		for (i = 0; nil[i]; i++)
+			pos = input_buf(buf, nil[i], pos);
+		return (i);
+	}
+
+	for (i = 0; s[i]; i++)
+		;
+	j = i - 1;
+	for (; j >= 0; j--)
+		pos = input_buf(buf, s[i], pos);
+
+	return (i);
+}
+
+/**
+ * print_rot - concatenates the string in ROT13
+ * @arguments: string
+ * @buf: the buffer
+ * @pos: the position to add on.
+ * Return: number of chars printed.
+ */
+int print_rot(va_list arguments, char *buf, unsigned int pos)
+{
+	char alf[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *str;
+	unsigned int i, j, k;
+	char nill[] = "(avyy)";
+
+	str = va_arg(arguments, char *);
+	if (str == NULL)
+	{
+		for (i = 0; nill[i]; i++)
+			pos = handl_buf(buf, nill[i], pos);
+		return (6);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		for (k = j = 0; alf[j]; j++)
+		{
+			if (str[i] == alf[j])
+			{
+				k = 1;
+				pos = handl_buf(buf, rot[j], pos);
+				break;
+			}
+		}
+		if (k == 0)
+			pos = input_buf(buf, str[i], pos);
+	}
+	return (i);
+}
